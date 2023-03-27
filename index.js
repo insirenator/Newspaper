811113cc101a8ac56adb7f6afd3c8e9cc0b3fbdc
insirenator <arsshakeeb149@protonmail.com>
2023-03-27 11:45:21 +0530

    Added: Multiple Article Support

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const hiddenContent = document.querySelector('.hidden-content');
-const readMoreLess = document.querySelector('.read-more-less');
+/*const hiddenContent = document.querySelector('.hidden-content');*/
+const readMoreLessList = Array.from(document.querySelectorAll('.read-more-less'));
 
-readMoreLess.addEventListener('click', () => {
-	if (hiddenContent.classList.contains('hide')){
-		hiddenContent.classList.remove('hide');
-		readMoreLess.textContent = "Read Less";
-	}
-	else {
-		hiddenContent.classList.add('hide');
-		readMoreLess.textContent = "Read More";
-	}
+readMoreLessList.forEach((readMoreLess) => {
+	readMoreLess.addEventListener('click', () => {
+
+		const hiddenContent = readMoreLess.parentElement.querySelector('.hidden-content');
+
+		if (hiddenContent.classList.contains('hide')){
+			hiddenContent.classList.remove('hide');
+			readMoreLess.textContent = "Read Less";
+		}
+		else {
+			hiddenContent.classList.add('hide');
+			readMoreLess.textContent = "Read More";
+		}
+	});
 });
